c1f1ca05a064497cd14e9d64320858d38355b4f2
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-08-08 19:54:57 +0300

    6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,16 +3,14 @@ var throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input');
 const messageInput = document.querySelector('textarea');
-const LOCALSTORAGE_KEY = 'feedback-form -state';
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
 
-form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('input', onFormInput);
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
-  event.preventDefault();
-
   formData[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
@@ -21,15 +19,12 @@ console.log(localData);
 console.log(form);
 
 function onPageReload() {
-  // event.preventDefault();
   //   console.log(form.firstElementChild.firstElementChild.value);
-  console.log(emailInput);
-  console.log(messageInput);
-  if (emailInput.value === '') {
-    emailInput.value = localData.email || '';
-  }
-  if (messageInput.value === '') {
-    messageInput.value = localData.message || '';
+  //   console.log(emailInput);
+  //   console.log(messageInput);
+  if (emailInput.value === '' || messageInput.value === '') {
+    emailInput.value = localData.email;
+    messageInput.value = localData.message;
   }
 }
 onPageReload();
@@ -54,3 +49,61 @@ function onFormSubmit(event) {
     localStorage.removeItem(LOCALSTORAGE_KEY);
   }
 }
+
+// var throttle = require('lodash.throttle');
+
+// const form = document.querySelector('.feedback-form');
+// const emailInput = document.querySelector('input');
+// const messageInput = document.querySelector('textarea');
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+// const formData = {};
+
+// form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormInput(event) {
+//   event.preventDefault();
+
+//   formData[event.target.name] = event.target.value;
+//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+//   console.log(formData[email]);
+// }
+// const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+// console.log(localData);
+// console.log(form);
+
+// function onPageReload() {
+//   // event.preventDefault();
+//   //   console.log(form.firstElementChild.firstElementChild.value);
+//   console.log(emailInput);
+//   console.log(messageInput);
+//   if (emailInput.value === '') {
+//     emailInput.value = localData.email || '';
+//   }
+//   if (messageInput.value === '') {
+//     messageInput.value = localData.message || '';
+//   }
+// }
+// onPageReload();
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const { email, message } = event.target.elements;
+//   const emailValue = email.value;
+//   const messageValue = message.value;
+//   const user = {
+//     emailValue,
+//     messageValue,
+//   };
+
+//   if (emailValue === '' || messageValue === '') {
+//     window.alert(
+//       "Please fill the form right now! Otherwise I'll call your mom!"
+//     );
+//   } else {
+//     console.log(user);
+//     form.reset();
+//     localStorage.removeItem(LOCALSTORAGE_KEY);
+//   }
+// }
