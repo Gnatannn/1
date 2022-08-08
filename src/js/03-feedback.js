44f301e50a8c3da79ca7c6e13882cfa23642d676
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-08-08 19:45:15 +0300

    4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,9 +12,7 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
   event.preventDefault();
-  formData[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
 }
 const localData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 console.log(localData);
